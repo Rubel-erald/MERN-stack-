206f94db953360513c5d361e6d4fd6c1fe0dfd4b
Rubel-erald <rubelerald.j@gmail.com>
2026-03-28 12:46:40 +0530

    datatypes&operators

---
+++ Javascript/datatypes_operators.js
@@ -0,0 +1,103 @@
+// types of variable in js
+
+// let
+// const
+// var 
+
+// // data types in js
+
+// string
+// Number
+// Boolean
+// undefined
+// null
+// Object
+// Array
+
+// two main types in js 
+// primitive
+
+// let a = 10;
+// let b = a;
+// b = 20;
+// console.log(a);
+// console.log(b);
+
+// - `a` and `b` are separate
+// - Changing `b` does NOT affect `a`
+
+// reference
+
+// object
+// array
+// function
+
+// let a = [1,2,3];
+// let b = a;
+// b.push(4);
+// console.log(a);
+// console.log(b);
+
+// OPERATORS IN JS
+// ARITHMETIC
+
+// let a = 10;
+// let b = 5;
+
+// console.log("Addition:", a + b);        // 15
+// console.log("Subtraction:", a - b);     // 5
+// console.log("Multiplication:", a * b);  // 50
+// console.log("Division:", a / b);        // 2
+// console.log("Modulus:", a % b);         // 0
+
+
+
+// Assignment Operators
+
+// let x = 10;
+
+// x += 5;   // x = x + 5
+// console.log(x); // 15
+
+// x -= 3;
+// console.log(x); // 12
+
+// x *= 2;
+// console.log(x); // 24
+
+// x /= 4;
+// console.log(x); // 6
+
+
+// Comparison Operators
+// let p = 10;
+// let q = "10";
+
+// console.log(p == q);   // true  (only value check)
+// console.log(p === q);  // false (value + type check)
+// console.log(p != q);   // false
+// console.log(p !== q);  // true
+// console.log(p > 5);    // true
+// console.log(p < 5);    // false
+
+
+// Logical Operators
+// let isLoggedIn = true;
+// let isAdmin = false;
+
+// console.log(isLoggedIn && isAdmin); // false
+// console.log(isLoggedIn || isAdmin); // true
+// console.log(!isLoggedIn);           // false
+
+// Increment / Decrement
+// let num = 5;
+
+// console.log(++num); // 6 (pre-increment)
+// console.log(num++); // 6 (post-increment)
+// console.log(num);   // 7
+
+// console.log(--num); // 6
+// console.log(num--); // 6
+// console.log(num);   // 5
+
+
