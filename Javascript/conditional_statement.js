d101dbf27d79e01199d0852c9cbf26f41427b673
Rubel-erald <rubelerald.j@gmail.com>
2026-03-28 12:58:08 +0530

    (if/else switch statement)

---
+++ Javascript/conditional_statement.js
@@ -0,0 +1,61 @@
+// (if / else & switch);
+// if statement
+// let age = 20;
+
+// if (age >= 18) {
+//   console.log("You are eligible to vote");
+// }
+
+// if..else statement 
+// let age = 16;
+
+// if (age >= 18) {
+//   console.log("Adult");
+// } else {
+//   console.log("Minor");
+// }
+
+
+// if...else if...else
+// let marks = 85;
+
+// if (marks >= 90) {
+//   console.log("Grade A");
+// } else if (marks >= 75) {
+//   console.log("Grade B");
+// } else if (marks >= 50) {
+//   console.log("Grade C");
+// } else {
+//   console.log("Fail");
+// }
+
+// NESTED IF
+// let age = 20;
+// let hasLicense = true;
+
+// if (age >= 18) {
+//   if (hasLicense) {
+//     console.log("You can drive");
+//   } else {
+//     console.log("Get a license first");
+//   }
+// } else {
+//   console.log("Too young to drive");
+// }
+
+// switch statement
+// let day = 3;
+
+// switch (day) {
+//   case 1:
+//     console.log("Monday");
+//     break;
+//   case 2:
+//     console.log("Tuesday");
+//     break;
+//   case 3:
+//     console.log("Wednesday");
+//     break;
+//   default:
+//     console.log("Invalid day");
+// }
