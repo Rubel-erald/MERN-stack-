a963f5639a3bb978dc335900bd7dd51cbf71bcd7
Rubel-erald <rubelerald.j@gmail.com>
2026-03-28 13:17:45 +0530

    loop statement

---
+++ Javascript/loops.js
@@ -0,0 +1,80 @@
+// for loop
+// Used when you know how many times to run
+// for (let i = 1; i <= 5; i++) {
+//   console.log("Number:", i);
+// }
+
+// while loop
+// Runs while condition is true
+// let i = 1;
+
+// while (i <= 5) {
+//   console.log("Count:", i);
+//   i++;
+// }
+
+// do...while Loop
+// Runs at least once, then checks condition
+// let i = 1;
+
+// do {
+//   console.log("Value:", i);
+//   i++;
+// } while (i <= 5);
+
+
+// for...of Loop (for Arrays)
+// Gives values
+
+// let arr = [10, 20, 30];
+
+// for (let value of arr) {
+//   console.log(value);
+// }
+
+
+// for...in Loop (for Objects / Keys)
+// Gives keys (indexes or object properties)
+
+// let person = {
+//   name: "Rubel",
+//   age: 23
+// };
+
+// for (let key in person) {
+//   console.log(key, ":", person[key]);
+// }
+
+
+// forEach() Method
+// Used with arrays
+
+// let numbers = [1, 2, 3];
+
+// numbers.forEach(function(num) {
+//   console.log(num);
+// });
+
+
+// break Statement
+
+// Stops the loop completely
+
+// for (let i = 1; i <= 5; i++) {
+//   if (i === 3) {
+//     break; stops loop when i = 3
+//   }
+//   console.log(i);
+// }
+
+
+// continue Statement
+// Skips current iteration and continues next
+
+// for (let i = 1; i <= 5; i++) {
+//   if (i === 3) {
+//     continue;  skip 3
+//   }
+//   console.log(i);
+// }
+
