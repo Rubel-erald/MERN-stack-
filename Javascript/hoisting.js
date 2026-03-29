5ca3db34c2c03f02b40a9bd27f79f811a2285135
Rubel-erald <rubelerald.j@gmail.com>
2026-03-29 12:47:40 +0530

    hoisting and its type

---
+++ Javascript/hoisting.js
@@ -0,0 +1,17 @@
+// hoisting in js
+// javascript reads the code first, then runs it
+// only declaration are hoisting, not value
+// => reads variables & function name first
+// => assigns values later
+
+// console.log(city);
+// let city = "coimbatore";
+// console.log(city);
+// const city2 = "coimbatore";
+
+// TDZ error
+
+// console.log(city);
+// var city3 = "coimbatore";
+
+// undefined
