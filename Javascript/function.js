3cd6b430571f9ae547dfd69fce8b4fcdcda95fb9
Rubel-erald <rubelerald.j@gmail.com>
2026-03-29 12:20:33 +0530

    functions and its types in js

---
+++ Javascript/function.js
@@ -0,0 +1,52 @@
+// declaring a function
+
+// function sum (a,b){
+//     return a + b;
+// }
+// let result = sum(2,2);
+// console.log(result);
+
+// function expression
+
+// const sum = function (a,b){
+//     return a + b;
+// }
+// sum(5,5);
+
+// arrow function
+
+// const sum = (a,b) => {
+//     return a + b;
+// }
+// sum(3,3);
+
+// anonymous function
+// setTimeout(() => {
+//     console.log("say hello after 2 second")
+// } , 2000);
+
+// (IIFE immediately invoked function)
+// (function () {
+//     console.log("hello");
+// })();
+
+
+// call back function
+
+// function calculate(a,b,callback){
+//     return callback(a,b);
+// }
+
+// function add(a,b){
+//     return a + b;
+// }
+// function sub (a,b){
+//     return a - b;
+// }
+// function multi (a,b){
+//     return a * b;
+// }
+// const result = calculate(10,5,sub);
+// console.log(result);
+
+
