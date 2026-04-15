ab3fd15054d1ba07c18ea5e2656b9e586529eda9
Rubel-erald <rubelerald.j@gmail.com>
2026-04-15 09:09:36 +0530

    For..in loop and for..of loop and object.key(),object.value(),object.entries()

---
+++ Javascript/ES6.js
@@ -0,0 +1,78 @@
+// ES6 features
+// => let , const , var
+// => arrow function 
+// => hoisting
+// => for..of for..in (object.key(),object.value(),object.entries())
+// => spread operator, rest operator
+// => template literal
+// => default parameters
+// => destructuring
+// => class syntax
+// => import and export
+// => promises
+// => enhanced object literal
+
+
+// for..of for..in
+// for...of (values)
+// Used for arrays / iterable values
+
+// let fruits1 = ["apple","mango","orange"];
+// for(let a of fruits1){
+//     console.log(a);
+// }
+
+
+
+//  for...in (keys / index)
+// Used for objects / array indexes
+
+// let fruits2 = ["apple","mango","orange"];
+// for(let b in fruits2){
+//     console.log(b);
+// }
+
+// let obj = { name: "rubel", age: 23 };
+
+// for (let key in obj) {
+//   console.log(key, obj[key]);
+// }
+
+
+// In array (of) used to get values of array
+// In array (in) used to get indexes of array
+
+// let see in object
+// let person1 = {
+//     name : "rubel",
+//     age : 23,
+//     place : "coimbatore"
+// }
+// for(let x in person1){
+//     console.log(x);
+// }
+
+// example of (object.key(),object.value(),object.entries())
+// let person2 = {
+//     name : "rubel",
+//     age : 23,
+//     place : "coimbatore"
+// }
+// console.log(Object.keys(person2));
+// console.log(Object.values(person2));
+// console.log(Object.entries(person2));
+
+// for(let a in Object.keys(person2)){
+//     console.log(a)
+// } //0,1,2
+
+// for(let a of Object.keys(person2)){
+//     console.log(a)
+// } // value
+
+// 🧠 Easy Memory Trick
+
+// 👉 "of = values"
+// 👉 "in = index / keys"
+
+
